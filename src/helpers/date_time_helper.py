518007982fc83ca3a0d5eff92f2e3834757b6aca
Mariia Radzhapova <mariiaradzhapova@gmail.com>
2024-12-01 18:10:28 +0200

    Remove extra space

--- src/helpers/date_time_helper.py
+++ src/helpers/date_time_helper.py
@@ -3,6 +3,5 @@ from datetime import date, datetime, timedelta
 def datetime_by_format(datetime_srt, datetime_format):
     return datetime.strptime(datetime_srt, datetime_format)
 
-
 def days_ago(days_count):
     return date.today() - timedelta(days=days_count)
