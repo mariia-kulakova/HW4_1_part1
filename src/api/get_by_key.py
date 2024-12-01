b9435c17a74ecd58179fc0f0f074538e0d3d5af1
Mariia Radzhapova <mariiaradzhapova@gmail.com>
2024-12-01 18:07:34 +0200

    Make case insensitive search by key

--- src/api/get_by_key.py
+++ src/api/get_by_key.py
@@ -6,5 +6,5 @@ router = APIRouter(tags=['Known CVEs'])
 @router.get('/get')
 def get_by_key(query):
     return [
-        cve for cve in retrieve_cves() if query in str(cve)
+        cve for cve in retrieve_cves() if query.lower() in str(cve).lower()
     ]
