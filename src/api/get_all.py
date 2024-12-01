980a5d722d853641b5b48a94f0d21800f5068a87
Mariia Radzhapova <mariiaradzhapova@gmail.com>
2024-12-01 17:43:46 +0200

    Add: get datetime by format in case dateAdded format changes from standard in the future

--- src/api/get_all.py
+++ src/api/get_all.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter
-from helpers.date_time_helper import days_ago
+from helpers.date_time_helper import days_ago, datetime_by_format
 from helpers.cves_helper import retrieve_cves
 
-DATE_FORMAT = '%Y-%m-%d'
+CVE_DATE_FORMAT = '%Y-%m-%d'
 MAX_CVES_COUNT = 40
 
 # 4 days ago + today = last 5 days
@@ -10,8 +10,10 @@ DAYS_AGO = 4
 
 router = APIRouter(tags=['All CVEs'])
 
-def is_recent(date_added_string):
-    return date_added_string > str(days_ago(DAYS_AGO))
+def is_recent(cve_date_added_str):
+    cve_date_added = datetime_by_format(cve_date_added_str, CVE_DATE_FORMAT).date()
+
+    return cve_date_added > days_ago(DAYS_AGO)
 
 @router.get('/get/all')
 def get_all():
